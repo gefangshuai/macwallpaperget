038a5c5ce8efdbbb0dd0e93708e17b677df59417
gefangshuai <gefangshuai@163.com>
2017-03-26 01:15:34 +0800

    完善图片保存逻辑，防止出现半图的情况

--- src/main/kotlin/io/gefangshuai/FetchService.kt
+++ src/main/kotlin/io/gefangshuai/FetchService.kt
@@ -1,8 +1,16 @@
 package io.gefangshuai
 
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import org.jsoup.Jsoup
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.net.URL
+import javax.imageio.ImageIO
+import java.io.FileOutputStream
+
+
 
 /**
  * Created by gefangshuai on 2017/3/25.
@@ -23,17 +31,29 @@ class FetchService {
         println("-------start fetch page $page--------")
         val currentUrl = getNextUrl()
         val document = Jsoup.connect(currentUrl).get()
-        val elements = document.select("div.pics>p>a").iterator()
+        val elements = document.select("img.hor").iterator()
         while (elements.hasNext()) {
-            val element = elements.next()
+            val img = elements.next()
+            if(img.attr("src").contains("ads"))
+                continue
+            val element = img.parent()
             val imgPageUrl = element.attr("abs:href")
             println("img page url: $imgPageUrl")
             val imgUrl = Jsoup.connect(imgPageUrl).get().body().select("div.pic-left>div>a").first().attr("abs:href")
             println("img url: $imgUrl")
             val filePath = "${output.removeSuffix("/")}/${imgUrl.substringAfterLast("/")}"
-            FileUtils.writeByteArrayToFile(File(filePath), Jsoup.connect(imgUrl).ignoreContentType(true).execute().bodyAsBytes(), false)
             println("file save: $filePath")
+            writeImg(imgUrl, filePath)
         }
         fetch(output)
+    }
+
+
+    fun writeImg(imgUrl: String, path: String){
+        val url = URL(imgUrl)
+        val inp = url.openStream()
+        val os = FileOutputStream(path)
+
+        IOUtils.copyLarge(inp, os)
     }
 }
